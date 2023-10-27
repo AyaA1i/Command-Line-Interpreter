ba2deff25e56d93bd7fed1bd0b4d95548494654c
Abdullah Mohammed <AbdullahFarg59@outlook.com>
2023-10-27 16:38:57 +0200

    add implementation  of Terminal and Parser

--- Parser.java
+++ Parser.java
@@ -1,37 +1,42 @@
 /**
- * The Parser class handles commands entered by the user that is parse it 
+ * The Parser class handles commands entered by the user that is parse it
  * into command name and argument list.
  */
 class Parser {
-	String commandName;
-	String[] args;
+    String commandName;
+    String[] args;
 
-	/**
-	 * Parses the user's input into cmd_name and arg_list.
-	 * 
-	 * @param input line to be parsed.
-	 * @return True if success, False otherwise. 
-	 */
-	public boolean parse(String input){
-		return false;
-	}
+    /**
+     * Parses the user's input into cmd_name and arg_list.
+     *
+     * @param input line to be parsed.
+     * @return True if success, False otherwise.
+     */
+    public boolean parse(String input)
+    {
+        String[] arr  = input.split(" ");
+        args = new String[arr.length-1];
+       for(int i = 1 ; i < arr.length ; i++)
+          args[i-1] = arr[i];
+       commandName = arr[0];
+        return false;
+    }
 
-	/**
-	 * Returns the command name
-	 * 
-	 * @return command name.
-	 */
-	public String getCommandName(){
-		return "";
-	}
+    /**
+     * Returns the command name
+     *
+     * @return command name.
+     */
+    public String getCommandName(){
+        return commandName;
+    }
 
-	/**
-	 * Returns the arguments list.
-	 *
-	 * @return arg list.
-	 */
-	public String[] getArgs(){
-		String[] s = {""};
-		return s;
-	}
+    /**
+     * Returns the arguments list.
+     *
+     * @return arg list.
+     */
+    public String[] getArgs(){
+        return args;
+    }
 }

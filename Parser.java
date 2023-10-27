069d7f8227a88398d569bc1468d88de8107bdde2
Ad7amstein <adham32003200@gmail.com>
2023-10-27 15:51:42 +0000

    FOLLOW BETTY CODING STYLE

--- Parser.java
+++ Parser.java
@@ -3,43 +3,41 @@
  * into command name and argument list.
  */
 class Parser {
-    String commandName;
-    String[] args;
+	String commandName;
+	String[] args;
 
-    /**
-     * Parses the user's input into cmd_name and arg_list.
-     *
-     * @param input line to be parsed.
-     * @return True if success, False otherwise.
-     */
-    public boolean parse(String input)
-    {
-        String[] arr  = input.split(" ");
-        args = new String[arr.length-1];
-       for(int i = 1 ; i < arr.length ; i++)
-          args[i-1] = arr[i];
-       commandName = arr[0];
-        return false;
-    }
+	/**
+	 * Parses the user's input into cmd_name and arg_list.
+	 *
+	 * @param input line to be parsed.
+	 * @return True if success, False otherwise.
+	 */
+	public boolean parse(String input)
+	{
+		String[] arr  = input.split(" ");
+		args = new String[arr.length-1];
+		for(int i = 1 ; i < arr.length ; i++)
+			args[i-1] = arr[i];
+		commandName = arr[0];
+		return false;
+	}
 
+	/**
+	 * 
+	 * Returns the command name
+	 *
+	 * @return command name.
+	 */
+	public String getCommandName(){
+		return commandName;
+	}
 
-
-    /**
-     * 
-     * Returns the command name
-     *
-     * @return command name.
-     */
-    public String getCommandName(){
-        return commandName;
-    }
-
-    /**
-     * Returns the arguments list.
-     *
-     * @return arg list.
-     */
-    public String[] getArgs(){
-        return args;
-    }
+	/**
+	 * Returns the arguments list.
+	 *
+	 * @return arg list.
+	 */
+	public String[] getArgs(){
+		return args;
+	}
 }
--- Terminal.java
+++ Terminal.java
@@ -12,34 +12,33 @@ import java.util.Scanner;
  **/
 public class Terminal {
 
-   public static  Parser parser =new Parser();
+	public static  Parser parser = new Parser();
 
-    /**
-     * main - Entry point.
-     * Creates a new parser object and allow the user to enter
-     * the input through the keyboard, untill he writes 'exit'.
-     * @param args an array of String arguments passed to the program
-     * @throws Exception if an error occurs while starting the store
-     */
-    public static void main(String[] args) throws Exception{
-        String cmd = "";
-        Scanner input = new Scanner(System.in);
+	/**
+	 * main - Entry point
+	 * Creates a new parser object and allow the user to enter
+	 * the input through the keyboard, untill he writes 'exit'.
+	 * @param args an array of String arguments passed to the program
+	 * @throws Exception if an error occurs while starting the store
+	 */
+	public static void main(String[] args) throws Exception{
+		String cmd = "";
+		Scanner input = new Scanner(System.in);
 
-        while (!cmd.equals("exit"))
-        {
-            System.out.print("> ");
-            cmd = input.nextLine();
-            if (cmd.equals("exit"))
-            {
-                System.out.println("Bye :)");
-                break;
-            }
-            parser.parse(cmd);
-            System.out.println(parser.getCommandName());
-            for(String i : parser.args)
-                System.out.println(i);
+		while (!cmd.equals("exit"))
+		{
+			System.out.print("> ");
+			cmd = input.nextLine();
+			if (cmd.equals("exit"))
+			{
+				System.out.println("Bye :)");
+				break;
+			}
+			parser.parse(cmd);
+			System.out.println(parser.getCommandName());
+			for(String i : parser.args)
+				System.out.println(i);
 
-        }
-
+		}
     }
 }

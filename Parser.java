07bc66bffd0d78bf00d9eac3db8d7ab6260f0506
Ad7amstein <adham32003200@gmail.com>
2023-10-27 10:37:24 +0000

    document parser class

--- Parser.java
+++ Parser.java
@@ -1,15 +1,35 @@
+/**
+ * The Parser class handles commands entered by the user that is parse it 
+ * into command name and argument list.
+ */
 class Parser {
 	String commandName;
 	String[] args;
 
+	/**
+	 * Parses the user's input into cmd_name and arg_list.
+	 * 
+	 * @param input line to be parsed.
+	 * @return True if success, False otherwise. 
+	 */
 	public boolean parse(String input){
 		return false;
 	}
 
+	/**
+	 * Returns the command name
+	 * 
+	 * @return command name.
+	 */
 	public String getCommandName(){
 		return "";
 	}
 
+	/**
+	 * Returns the arguments list.
+	 *
+	 * @return arg list.
+	 */
 	public String[] getArgs(){
 		String[] s = {""};
 		return s;

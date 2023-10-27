451658bb6de53db65d589b9b19a54f3ab99ccfa8
Ad7amstein <adham32003200@gamil.com>
2023-10-27 10:27:54 +0000

    Initial parser class

---
+++ Parser.java
@@ -0,0 +1,17 @@
+class Parser {
+	String commandName;
+	String[] args;
+
+	public boolean parse(String input){
+		return false;
+	}
+
+	public String getCommandName(){
+		return "";
+	}
+
+	public String[] getArgs(){
+		String[] s = {""};
+		return s;
+	}
+}
